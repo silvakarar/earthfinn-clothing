b26253c6c3c08376918e6c811b98372ecc5b704b
mark appleyard <markappleyard@marks-iMac.local>
2021-05-05 09:50:50 -0700

    created project with create react app and added files for homepage

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+// SASS
+import './HomePage.styles.scss'
+
+const HomePage = () => (
+  <div className="Homepage">
+    <div className="Homepage-directory-menu">
+      <div className="Homepage-menu-item">
+        <div className="Homepage-content">
+          <h1 className="Homepage-title">HATS</h1>
+          <span className="Homepage-subtitle">SHOP NOW</span>
+        </div>
+            </div>
+            <div className="Homepage-menu-item">
+        <div className="Homepage-content">
+          <h1 className="Homepage-title">JACKETS</h1>
+          <span className="Homepage-subtitle">SHOP NOW</span>
+        </div>
+            </div>
+            <div className="Homepage-menu-item">
+        <div className="Homepage-content">
+          <h1 className="Homepage-title">SNEAKERS</h1>
+          <span className="Homepage-subtitle">SHOP NOW</span>
+        </div>
+            </div>
+            <div className="Homepage-menu-item">
+        <div className="Homepage-content">
+          <h1 className="Homepage-title">MENS</h1>
+          <span className="Homepage-subtitle">SHOP NOW</span>
+        </div>
+            </div>
+            <div className="Homepage-menu-item">
+        <div className="Homepage-content">
+          <h1 className="Homepage-title">WOMENS</h1>
+          <span className="Homepage-subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
